01ab667828725553c8a77db9a0be1f62dc00c36e
Renan <renancossenzo@hotmail.com>
2025-11-03 21:45:13 -0300

    Use jsonify to return data

--- back/controllers/generator_controller.py
+++ back/controllers/generator_controller.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
 from services.generator_service import IGeneratorService
 
 generator_bp = Blueprint("generator", __name__)
@@ -9,10 +9,8 @@ def create_generator_routes(generator_service: IGeneratorService):
     def generate_paragraphs(quantity: int):
         paragraphResult = generator_service.generate_paragraphs(quantity)
         if isinstance(paragraphResult, str):
-            return {
-                "error": paragraphResult
-            }, 400
+            return jsonify(error=paragraphResult), 400
         
-        return { "paragraphs": paragraphResult }, 200
+        return jsonify(paragraphs=paragraphResult), 200
     
     return generator_bp

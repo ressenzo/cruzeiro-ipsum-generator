2037aab0f9fb2a6427f2f990c4d7ef13d8f8281f
Renan <renancossenzo@hotmail.com>
2025-11-04 20:47:03 -0300

    Solve pylint warnings

--- back/main.py
+++ back/main.py
@@ -1,18 +1,20 @@
+"""Main entrypoint"""
+
 from flask import Flask
 
 from services.generator_service import GeneratorService
 from controllers.generator_controller import create_generator_routes
 
 
-def create_app():
-    app = Flask(__name__)
+def __create_app():
+    flask = Flask(__name__)
     generator_service = GeneratorService()
-    app.register_blueprint(
+    flask.register_blueprint(
         create_generator_routes(generator_service)
     )
-    return app
+    return flask
 
 
 if __name__ == "__main__":
-    app = create_app()
+    app = __create_app()
     app.run(debug=True)
